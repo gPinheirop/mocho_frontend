f1421361f932af46aee87a72abfe6a741180c28f
gPinheirop <gabrielpedreira_@hotmail.com>
2024-06-04 21:06:34 -0300

    config axios and token store functions

---
+++ src/services/httpservice.ts
@@ -0,0 +1,71 @@
+import axios, {
+  RawAxiosRequestHeaders,
+  AxiosError,
+  RawAxiosResponseHeaders,
+} from "axios";
+import { useStore } from "@/store";
+
+export const api = axios.create({
+  baseURL: "add your backend base url here",
+});
+
+type Params = {
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  url: string;
+  body?: any;
+  headers: RawAxiosRequestHeaders;
+};
+
+export type HttpResponse<T, E = unknown> =
+  | {
+      hasError: true;
+      error: AxiosError<E>;
+    }
+  | {
+      hasError: false;
+      data: T;
+      headers: RawAxiosResponseHeaders & {
+        [key: string]: any;
+      };
+    };
+
+const defaultHeaders: RawAxiosRequestHeaders = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+export const getHttpHeaders = {
+  withoutAuthToken: async () => defaultHeaders,
+  withAuthToken: async () => {
+    const authorization = "Bearer " + useStore.getState().accessToken;
+    return {
+      ...defaultHeaders,
+      Authorization: authorization,
+    };
+  },
+};
+
+export async function httpRequest<T, E = unknown>({
+    method,
+    url,
+    body,
+    headers,
+  }: Params): Promise<HttpResponse<T, E>> {
+    try {
+      const response = await api<T>({method, url, data: body, headers});
+
+      return {
+        hasError: false,
+        data: response.data,
+        headers: response.headers,
+      };
+    } catch (error: any) {
+      if (error.response.data.code === 'token_not_valid') {
+        useStore.getState().deleteToken();
+      }
+      return {
+        hasError: true,
+        error,
+      };
+    }
+  }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,26 @@
 import { create } from "zustand";
 export type Store = {
-
+  accessToken: string;
+  deleteToken: () => void
+  loginLoading: boolean;
+  login: (email: string, password: string) => Promise<void>;
 };
 
-export const useStore = create<Store>((set)=> ({
-    // add states and their update functions
-    // example : 0,
-    // updateState: () => set((state) => ({ example: state.example + 1 })),
-}))
+export const useStore = create<Store>((set) => ({
+  // add states and their update functions
+  // example : 0,
+  // updateState: () => set((state) => ({ example: state.example + 1 })),
+  accessToken: "",
+  updateAccessToken: () => {},
+  deleteToken: () => set({ accessToken: "" }),
+  loginLoading: false,
+  login: async (email: string, password: string) => {
+    set({loginLoading: true});
+    // create login api call
+    // set({
+    //   accessToken: result.data.access,
+    //   loginLoading: false,
+    // });
+  },
+
+}));
