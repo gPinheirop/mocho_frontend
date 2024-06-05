dc23089193ed6bfe3b87063c11450e4316ff2580
gPinheirop <gabrielpedreira_@hotmail.com>
2024-06-05 15:56:04 -0300

    add radix ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginScreen from "./member/screens/LoginScreen/index.tsx";
+import { Theme } from "@radix-ui/themes";
+import '@radix-ui/themes/styles.css';
 
 const router = createBrowserRouter([
   {
@@ -17,6 +19,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <Theme accentColor="blue" radius="small">
+      <RouterProvider router={router}></RouterProvider>
+    </Theme>
   </React.StrictMode>
 );
--- src/member/screens/LoginScreen/index.tsx
+++ src/member/screens/LoginScreen/index.tsx
@@ -1,16 +1,33 @@
+import { Button, Flex, TextField } from "@radix-ui/themes";
 import logo from "../../../assets/logo.png";
 import "./styles.css";
 
 function LoginScreen() {
   return (
     <div className="page-container">
-      <div className="login-container">
+      <Flex
+        direction={"column"}
+        gap={"3"}
+        className="login-container"
+        p={"6"}
+        m={"9"}
+      >
         <img className="logo" src={logo} alt="Mocho" />
-        {/* <Input className="login-input" type="email" placeholder="Email" />
-        <Input className="login-input" type="password" placeholder="Senha" />
-        <Button className="login-buttton">Login</Button> */}
-      </div>
+        <TextField.Root size="2" placeholder="Email" />
+        <TextField.Root size="2" placeholder="Senha" type="password" />
+        <Button variant="soft" className="login-buttton">
+          Login
+        </Button>
+      </Flex>
     </div>
+
+    // <div className="page-container">
+    //   <div className="login-container">
+    //     <input className="login-input" type="email" placeholder="Email" />
+    //     <input className="login-input" type="password" placeholder="Senha" />
+    //     <button className="login-buttton">Login</button>
+    //   </div>
+    // </div>
   );
 }
 
